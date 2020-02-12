b3aca967aa3dcedba4b6882c8bea96533b1ef8d6
Massimo Bortolamei <massimo.bortolamei@hegias.com>
2020-02-12 17:01:19 +0100

    Revert "Merge branch 'look-controls-upgrade' into 0.9.2"
    This reverts commit fe74ec95043f9f46b807723748bba4fa916adcb6.

--- src/index.js
+++ src/index.js
@@ -89,7 +89,7 @@ require('./core/a-mixin');
 require('./extras/components/');
 require('./extras/primitives/');
 
-console.log('A-Frame Version: 0.9.2 (Date 2020-02-12, Commit #e8e72bf1)');
+console.log('A-Frame Version: 0.9.2 (Date 2019-10-21, Commit #528d681e)');
 console.log('three Version (https://github.com/supermedium/three.js):',
             pkg.dependencies['super-three']);
 console.log('WebVR Polyfill Version:', pkg.dependencies['webvr-polyfill']);
